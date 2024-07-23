1cf7ee3e93d5e4666e0b8ed47d108b7bacd6d679
WilliamSoares21 <pedro.williamsoares2022@gmail.com>
2024-07-23 19:15:49 -0300

    Add conteúdo do módulo de métodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -7,9 +7,14 @@ public class TiposVariaveis {
         String meuNome = "William Soares";
         double salarioMinimo = 1414;
 
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //exemplo de uso do cast, que é a conversão de objetos ou tipos primitivos de um tipo para outro tipo. Ao se tratar casting, considera-se a necessidade de conversão explícita entre tipos – o que, potencialmente, pode causar perdas de informação.
+
+        System.out.println(numeroCurto2);
 
        int numero = 5;
 
@@ -17,8 +22,9 @@ public class TiposVariaveis {
 
        System.out.println(numero);//valor foi alterado
 
-       final double VALOR_DE_PI = 3.14; //sempre que você definir uma variável que não pode ser alterada, lembrar de colocar em caixa alta(o nome da variável) e inserir a palavra "final" antes do tipo da váriavel para informar que aquela variável é um constante 
+       final double VALOR_DE_PI = 3.14; //sempre que você definir uma variável que não pode ser alterada, lembrar de colocar em caixa alta(o nome da variável) e inserir a palavra "final" antes do tipo da váriavel para informar que aquela variável é um constante.
 
+       System.out.println(VALOR_DE_PI);
 
 
 
